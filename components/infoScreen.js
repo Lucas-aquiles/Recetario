ab66f56644992ea67ac556f744a9230ecc30bb08
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-01-17 23:58:34 -0300

    finish

--- components/infoScreen.js
+++ components/infoScreen.js
@@ -14,6 +14,7 @@ import { Entypo, Octicons, AntDesign } from "@expo/vector-icons";
 
 const InfoScreen = ({ route, navigation }) => {
   const item = route.params;
+  console.log("holaaaaaaaa",item)
 
   return (
     <View style={styles.container}>
--- components/search-bar.js
+++ components/search-bar.js
@@ -1,7 +1,6 @@
-import * as React from "react";
+import React, { useEffect } from "react";
+import data from "../resources/data.json"
 import { Searchbar } from "react-native-paper";
-// import { StyleSheet } from 'react-native';
-import { styles } from "../styles/styleApp";
 import {
   View,
   Text,
@@ -9,11 +8,34 @@ import {
   StyleSheet,
   StatusBar,
   Platform,
+  
 } from "react-native";
 const SearchBarComponent = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
+  const [dataJson, setdataJson] = React.useState([])
+  const [resultSearch,setResultSearch] = React.useState("")
   const onChangeSearch = (query) => setSearchQuery(query);
-  console.log(searchQuery)
+
+  console.log(dataJson)
+  console.log(resultSearch)
+
+  useEffect(() => {
+    if(data){
+      let result = data.dishes.map(e=> ({id: e.id , title: e.title}) )
+      setdataJson(result)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (dataJson){  
+    const result = dataJson?.find( e => e.title.slice(0,3) === searchQuery.trim())
+    setResultSearch(result)
+    }
+  }, [searchQuery])
+  
+  
+  
+
   return (
     <View>   
     <Searchbar
@@ -21,8 +43,21 @@ const SearchBarComponent = () => {
       onChangeText={onChangeSearch}
       value={searchQuery}
     />
+    { resultSearch !== undefined ?(  
+    <Text style={styles.conta} 
+   >
+      {resultSearch.title}
+    </Text>):(null)}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  conta: {
+    borderWidth: 1,
+    padding: 20,
+    backgroundColor:"white",
+    color:"red"
+  },
+});
 
 export default SearchBarComponent;
